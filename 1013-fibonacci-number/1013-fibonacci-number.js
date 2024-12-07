94337532da3a93638e61db4cffba1344de0249c5
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2024-12-07 03:48:11 -0500

    Time: 62 ms (44.07%), Space: 48.6 MB (67.07%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -0,0 +1,15 @@
+var fib = function (n) {
+  if (n == 0) return 0;
+
+  let prev = 0,
+    cur = 1,
+    temp;
+
+  for (let i = 1; i < n; i++) {
+    temp = cur;
+    cur = cur + prev;
+    prev = temp;
+  }
+
+  return cur;
+};
