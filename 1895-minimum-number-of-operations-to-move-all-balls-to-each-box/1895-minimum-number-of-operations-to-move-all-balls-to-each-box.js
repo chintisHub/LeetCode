3e6d2eab6a4f79803641ddd517492409c3690d1d
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2025-01-05 23:21:12 -0500

    Time: 219 ms (12.62%), Space: 55.3 MB (11.65%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -1,34 +1,44 @@
-/**
- * @param {string} boxes
- * @return {number[]}
- */
-var minOperations = function(boxes) {
-    const n = boxes.length;
-    const distances = new Array(n).fill(0);
-
-    let prefixCount = 0;
-    let prefixSum = 0;
-
-    // Calculate prefix sums
-    for (let i = 0; i < n; i++) {
-        distances[i] = prefixCount * i - prefixSum;
+var minOperations = function (boxes) {
+    function leftSum(arr) {
+        let sum = 0;
+        let counter = 0;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            counter++;
+            if (arr[i] === '1') {
+                sum += counter
+            }
+        }
+        return sum;
+    }
+    function rightSum(arr) {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === '1') {
+                sum += i + 1;
+            }
+        }
+        return sum;
+    }
+    let count = 0;
+    let revCount = 0;
+    for (let i = 0; i < boxes.length; i++) {
         if (boxes[i] === '1') {
-            prefixCount++;
-            prefixSum += i;
+            count += i;
+            revCount += Math.abs(i - (boxes.length - 1))
+        }
+
+    }
+    let res = []
+    for (let i = 0; i < boxes.length; i++) {
+        if (i !== 0 && i !== boxes.length - 1) {
+            let leftArr = boxes.slice(0, i);
+            let rightArr = boxes.slice(i + 1, boxes.length);
+            res.push(leftSum(leftArr) + rightSum(rightArr));
+        } else if (i === 0) {
+            res.push(count);
+        } else {
+            res.push(revCount)
         }
     }
-
-    let suffixCount = 0;
-    let suffixSum = 0;
-
-    // Calculate suffix sums and update the distances array
-    for (let i = n - 1; i >= 0; i--) {
-        distances[i] += suffixSum - suffixCount * i;
-        if (boxes[i] === '1') {
-            suffixCount++;
-            suffixSum += i;
-        }
-    }
-
-    return distances;
+    return res
 };
