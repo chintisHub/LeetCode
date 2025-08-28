7a16aa9738efc1b340ea4495b909e413dd974261
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2025-08-29 01:19:52 +0530

    Time: 2 ms (99.17%), Space: 56.9 MB (73.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,15 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length !==t.length) return false;
+    const counter = new Array(26).fill(0);
+    for (let i =0; i< s.length; i++){
+        counter[s.charCodeAt(i)-97]++;
 
-    const count =[]
-    for(let char of s){
-        count[char] = (count[char] || 0)+1;
     }
-    for(let char of t){
-        if(!count[char]) return false;
-        count[char]--;
+
+    for(let i =0; i<t.length; i++){
+        counter[t.charCodeAt(i)-97]--;
     }
-    return true;
+
+    for(let i =0; i<26; i++){
+        if(counter[i]!= 0){
+            return false;
+        }
+    }
+        return true;
+
 };
