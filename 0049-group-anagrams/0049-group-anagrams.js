c47783762336cc51d899f01ca4db1104d768993a
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2025-08-29 17:37:48 +0530

    Time: 23 ms (90.4%), Space: 69.5 MB (17.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,25 +1,9 @@
-/**
- * @param {string[]} strs
- * @return {string[][]}
- */
 var groupAnagrams = function (strs) {
-    let arr = [];
-    let myMap = new Map();
-    if (strs.length < 2) {
-        arr.push(strs);
-        return arr;
-    }
-
-    for (let i = 0; i < strs.length; i++) {
-        myMap.set(strs[i].split("").sort().join(""), []);
-    }
-    for (let i = 0; i < strs.length; i++) {
-        let valueArr = myMap.get(strs[i].split("").sort().join(""));
-        valueArr.push(strs[i]);
-        myMap.set(strs[i].split("").sort().join(""), valueArr);
-    }
-    for (let [key, value] of myMap) {
-        arr.push(value);
-    }
-    return arr
+  const groups = new Map();
+  for (const s of strs) {
+    const key = s.split("").sort().join(""); // O(k log k)
+    if (!groups.has(key)) groups.set(key, []);
+    groups.get(key).push(s);
+  }
+  return Array.from(groups.values());
 };
