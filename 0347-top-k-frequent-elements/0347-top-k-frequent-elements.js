ec4a75092ebe43c867f3b0b9ab5a6a69a63064d9
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2025-08-31 03:34:33 +0530

    Time: 13 ms (52.02%), Space: 63.4 MB (15.45%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const freq = new Map();
+    for(let num of nums){
+        freq.set(num, (freq.get(num) || 0)+1);
+    }
+
+    const buckets = Array(nums.length+1).fill().map(()=>[]);
+
+    for(let [key,val] of freq){
+        buckets[val].push(key);
+    }
+ const res = [];
+    for (let i = buckets.length - 1; i >= 0 && res.length < k; i--) {
+        for (let num of buckets[i]) {
+            res.push(num);
+            if (res.length === k) return res;
+        }
+    }
+    return res;
+};
