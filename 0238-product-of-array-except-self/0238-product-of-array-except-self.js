1f2dac6f3314dd7d836a1ddb1dbd72d435c43623
Sai Niranjan Nallam <nallamniranjan@gmail.com>
2025-09-04 23:42:53 +0530

    Time: 10 ms (39.77%), Space: 69.7 MB (71.85%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -1,12 +1,14 @@
 var productExceptSelf = function(nums) {
     let n = nums.length;
     let ans = new Array(n).fill(1);
-
+    console.log(nums);
+    console.log(ans)   
     // Calculate prefix products
     for (let i = 1; i < n; i++) {
         ans[i] = ans[i - 1] * nums[i - 1];
     }
 
+    console.log(ans)   
 
 
     let prevSuffix = 1;
@@ -15,6 +17,8 @@ var productExceptSelf = function(nums) {
         prevSuffix *= nums[i + 1];
         ans[i] *= prevSuffix;
     }
+    console.log(ans)   
+
 
     return ans;
 };
